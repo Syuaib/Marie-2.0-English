801fdf775c1aea5181f62d84229f276288dfe5bc
Syuaib <47686826+Syuaib@users.noreply.github.com>
2020-05-26 19:41:22 +0800

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,7 +28,6 @@ I'm a group manager bot maintained by [this person](tg://user?id={}).
 
 For more commands click /help...
 
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
 
 """
 
